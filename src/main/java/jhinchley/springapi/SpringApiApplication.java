31bd61f14558f04172bbbb5199007a81c1b93e21
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2022-01-09 00:33:24 -0600

    jpa magically injects controllers, I wish DI was more explicit

--- src/main/java/jhinchley/springapi/SpringApiApplication.java
+++ src/main/java/jhinchley/springapi/SpringApiApplication.java
@@ -1,20 +1,17 @@
 package jhinchley.springapi;
 
+//base spring
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+//jpa repos
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@RestController
+@EnableJpaAuditing
 public class SpringApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringApiApplication.class, args);
-	}
-
-	@RequestMapping("/")
-	public String homeEndpoint(){
-		return "hello spring from a docker container now with compose";
 	}
 }
